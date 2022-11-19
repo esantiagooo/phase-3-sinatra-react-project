31c52957fff0af9360940b94713fb4772892b3aa
Eric <eric.yanez50@qmail.cuny.edu>
2022-11-18 19:51:34 -0500

    modified db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,13 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+10.times do
+    Roommate.create(
+        firstname: Faker::Name.firstname,
+        lastname: Faker::Name.lastname,
+        gender: Faker::Gender.type.gender
+        age: rand(18..60)
+    )
+end 
+
 puts "✅ Done seeding!"
