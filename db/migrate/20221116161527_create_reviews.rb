72554a40f01756ad61bfb21b54b8baeaccfe6d20
Eric <eric.yanez50@qmail.cuny.edu>
2022-11-20 09:25:31 -0500

    modified :  db/migrate/20221116161527_create_reviews.rb

--- db/migrate/20221116161527_create_reviews.rb
+++ db/migrate/20221116161527_create_reviews.rb
@@ -1,10 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :vibes
-      t.integer :neatness
-      t.integer :responsible
       t.integer :friendly
+      t.integer :clean
+      t.integer :vibes
       t.integer :foodthief
       t.string :comment
       t.integer :roommate_id
